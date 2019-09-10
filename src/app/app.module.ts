2da11bda994ecd9e8a33fe9aed5dfb8af5cd96cd
unknown <Andrew Miller@SCH_IT_TECH3.schospital.local>
2019-09-10 16:44:18 -0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AppViewComponent } from './app-view/app-view.component';
+
+/*const appRoutes: Routes = [
+  { path: '', component: root },
+  { path: 'apps', component: appView },
+  { path: 'memories', component: memories },
+  { path: '**', component: PageNotFoundComponent }
+];*/
+
+const appRoutes: Routes = [
+  //{ path: '', redirectTo: '/app-view', pathMatch: 'full' },
+  //{ path: 'app-view', component: AppViewComponent }
+];
 
 @NgModule({
   declarations: [
@@ -10,8 +24,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/assets/scripts/waveform.js
@@ -0,0 +1,12 @@
+
+console.log('working!');
+
+/*$(window).resize(function(){
+  var width = $(window).width();
+  var ratio = width/1680;
+  var waveWidth = width * ratio;
+
+  $('#waveform').css('width', waveWidth);
+  console.log(waveWidth);
+});
+*/
